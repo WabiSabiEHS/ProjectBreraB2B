c771751ab17e6ed6144b89781fea35f2d5920ae3
Tasogare_no_Jack <jacopoalberti363@gmail.com>
2023-11-22 17:19:26 +0100

    Player Movement to test

---
+++ Assets/Scripts/PlayerMovementComponent.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementComponent : MonoBehaviour
+{
+    [SerializeField, Range(1, 10)] private float speed = 2f;
+
+    private void Update()
+    {
+        Vector3 direction = new Vector3(InputManager.MovementAxis.x, 0, InputManager.MovementAxis.y);
+        transform.position += direction * (speed * Time.deltaTime);
+    }
+}
