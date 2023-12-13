e303eb8e6c89e4a50a8c85a8ef47a93552205a01
Tasogare_no_Jack <jacopoalberti363@gmail.com>
2023-12-14 00:18:44 +0100

    Main Build updates

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -16,9 +16,21 @@ public class PlayerManager : MonoBehaviour
     public void TogglePlayerUI(object[] param)
     {
         if (PlayerUI.activeInHierarchy)
+        {
             PlayerUI.SetActive(false);
+            CanPlayerMove = false;
+        }
+
 
         else if (!PlayerUI.activeInHierarchy)
+        {
             PlayerUI.SetActive(true);
+            CanPlayerMove = true;
+        }
+    }
+
+    public void TogglePlayerMovement(bool toggle)
+    {
+        CanPlayerMove = toggle;
     }
 }
