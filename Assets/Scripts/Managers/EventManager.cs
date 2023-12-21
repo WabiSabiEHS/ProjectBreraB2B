3a5a40a315c46deebf8bad2ae2981b255de39f0a
Tasogare_no_Jack <jacopoalberti363@gmail.com>
2023-12-21 10:10:42 +0100

    Added TDD

--- Assets/Scripts/Managers/EventManager.cs
+++
@@ -1,9 +0,0 @@
-using Framework.Generics.Pattern.ObserverPattern;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EventManager : ObserverPattern<string>
-{
-
-}
